ce83d8611978097aa18383a98f8d3936670cd9fb
Évelin de Souza <evelinsouza.3113@gmail.com>
2026-09-23 17:21:26 -0300

    Add files via upload

---
+++ pratica2/src/auditoria/auditoria.entity.ts
@@ -0,0 +1,34 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn
+} from 'typeorm';
+
+@Entity({ name: 'auditorias' })
+export class Auditoria {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ name: 'ator_id', type: 'int' })
+  atorId: number;
+
+  @Column({ type: 'varchar', length: 50 })
+  acao: string;
+
+  @Column({ name: 'recurso_tipo', type: 'varchar', length: 50 })
+  recursoTipo: string;
+
+  @Column({ name: 'recurso_id', type: 'int' })
+  recursoId: number;
+
+  @Column({ name: 'centroCusto_id', type: 'int' })
+  centroCustoId: number;
+
+  @Column({ type: 'jsonb', nullable: true })
+  detalhes: Record<string, unknown> | null;
+
+  @CreateDateColumn({ name: 'criada_em', type: 'timestamptz'})
+  criadaEm: Date;
+
+}
